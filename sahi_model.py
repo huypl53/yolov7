75ca281eb8e1bdb7a1e8558b1320042212862bc5
le huy <37492649+huypl53@users.noreply.github.com>
2022-12-22 23:17:10 +0700

    init

---
+++ sahi_model.py
@@ -0,0 +1,279 @@
+# OBSS SAHI Tool
+# Code written by Fatih C Akyon, 2020.
+
+import logging
+from typing import Any, Dict, List, Optional
+
+import numpy as np
+
+from sahi.models.base import DetectionModel
+from sahi.prediction import ObjectPrediction
+from sahi.utils.compatibility import fix_full_shape_list, fix_shift_amount_list
+from sahi.utils.import_utils import check_package_minimum_version, check_requirements
+
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+logger = logging.getLogger(__name__)
+
+_category_names = ['bike', 'car']
+
+class Yolov7DetectionModel(DetectionModel):
+    def check_dependencies(self) -> None:
+        check_requirements(["torch"])
+
+    def load_model(self, image_width, image_height):
+        """
+        Detection model is initialized and set to self.model.
+        """
+
+        # import yolov5
+        assert image_width == image_height
+
+        try:
+            # model = yolov5.load(self.model_path, device=self.device)
+            model = attempt_load(self.model_path, map_location=self.device)  # load FP32 model
+
+            self.set_model(model)
+
+            # Run inference
+            if self.device != 'cpu':
+                model(torch.zeros(1, 3, image_width, image_height).to(self.device).type_as(next(model.parameters())))  # run once
+
+        except Exception as e:
+            raise TypeError("model_path is not a valid yolov5 model path: ", e)
+
+    def set_model(self, model: Any):
+        """
+        Sets the underlying YOLOv5 model.
+        Args:
+            model: Any
+                A YOLOv5 model
+        """
+
+        # if model.__class__.__module__ not in ["yolov5.models.common", "models.common"]:
+        #     raise Exception(f"Not a yolov5 model: {type(model)}")
+
+        model.conf = self.confidence_threshold
+        self.model = model
+
+        # set category_mapping
+        if not self.category_mapping:
+            category_mapping = {str(ind): category_name for ind, category_name in enumerate(self.category_names)}
+            self.category_mapping = category_mapping
+
+    def perform_inference(self, image: np.ndarray):
+        """
+        Prediction is performed using self.model and the prediction result is set to self._original_predictions.
+        Args:
+            image: np.ndarray
+                A numpy array that contains the image to be predicted. 3 channel image should be in RGB order.
+        """
+
+        # # Confirm model is loaded
+        if self.model is None:
+            raise ValueError("Model is not loaded, load it by calling .load_model()")
+        # if self.image_size is not None:
+
+        #     prediction_result = self.model(image, size=self.image_size)
+        # else:
+        #     prediction_result = self.model(image)
+
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+        t0 = time.time()
+        for path, img, im0s, vid_cap in dataset:
+            img = torch.from_numpy(img).to(device)
+            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+
+            # Warmup
+            # if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            #     old_img_b = img.shape[0]
+            #     old_img_h = img.shape[2]
+            #     old_img_w = img.shape[3]
+            #     for i in range(3):
+            #         model(img, augment=opt.augment)[0]
+
+            # Inference
+            t1 = time_synchronized()
+            with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+                # pred = model(img, augment=opt.augment)[0]
+                pred = model(img)[0]
+            t2 = time_synchronized()
+
+            # Apply NMS
+            pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+            t3 = time_synchronized()
+
+            # Apply Classifier
+            # if classify:
+            #     pred = apply_classifier(pred, modelc, img, im0s)
+
+            # Process detections
+            for i, det in enumerate(pred):  # detections per image
+                # if webcam:  # batch_size >= 1
+                #     p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+                # else:
+                #     p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+                p, s, im0, frame = path, '', im0s, 'frame'
+                object_prediction_list = []
+
+                # p = Path(p)  # to Path
+                # save_path = str(save_dir / p.name)  # img.jpg
+                # txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                    # Print results
+                    # for c in det[:, -1].unique():
+                    #     n = (det[:, -1] == c).sum()  # detections per class
+                        # s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        if save_txt:  # Write to file
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                            line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                            with open(txt_path + '.txt', 'a') as f:
+                                f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                        # if save_img or view_img:  # Add bbox to image
+                        #     label = f'{names[int(cls)]} {conf:.2f}'
+                        #     plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+
+                # Print time (inference + NMS)
+                # print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+
+
+                        object_prediction = ObjectPrediction(
+                            bbox=bbox,
+                            category_id=category_id,
+                            score=score,
+                            bool_mask=None,
+                            category_name=category_name,
+                            shift_amount=shift_amount,
+                            full_shape=full_shape,
+                        )
+                        object_prediction_list.append(object_prediction)
+                    object_prediction_list_per_image.append(object_prediction_list)
+
+        self._object_prediction_list_per_image = object_prediction_list_per_image
+
+    @property
+    def num_categories(self):
+        """
+        Returns number of categories
+        """
+        return len(_category_names)
+
+    @property
+    def has_mask(self):
+        """
+        Returns if model output contains segmentation mask
+        """
+        # import yolov5
+        # from packaging import version
+
+        # if version.parse(yolov5.__version__) < version.parse("6.2.0"):
+        #     return False
+        # else:
+        #     return False  # fix when yolov5 supports segmentation models
+        return False
+
+    @property
+    def category_names(self):
+        # if check_package_minimum_version("yolov5", "6.2.0"):
+        #     return list(self.model.names.values())
+        # else:
+        #     return self.model.names
+        return _category_names
+        
+
+    def _create_object_prediction_list_from_original_predictions(
+        self,
+        shift_amount_list: Optional[List[List[int]]] = [[0, 0]],
+        full_shape_list: Optional[List[List[int]]] = None,
+    ):
+        """
+        self._original_predictions is converted to a list of prediction.ObjectPrediction and set to
+        self._object_prediction_list_per_image.
+        Args:
+            shift_amount_list: list of list
+                To shift the box and mask predictions from sliced image to full sized image, should
+                be in the form of List[[shift_x, shift_y],[shift_x, shift_y],...]
+            full_shape_list: list of list
+                Size of the full image after shifting, should be in the form of
+                List[[height, width],[height, width],...]
+        """
+        original_predictions = self._original_predictions
+
+        # compatilibty for sahi v0.8.15
+        shift_amount_list = fix_shift_amount_list(shift_amount_list)
+        full_shape_list = fix_full_shape_list(full_shape_list)
+
+        # handle all predictions
+        object_prediction_list_per_image = []
+        for image_ind, image_predictions_in_xyxy_format in enumerate(original_predictions.xyxy):
+            shift_amount = shift_amount_list[image_ind]
+            full_shape = None if full_shape_list is None else full_shape_list[image_ind]
+            object_prediction_list = []
+
+            # process predictions
+            for prediction in image_predictions_in_xyxy_format.cpu().detach().numpy():
+                x1 = prediction[0]
+                y1 = prediction[1]
+                x2 = prediction[2]
+                y2 = prediction[3]
+                bbox = [x1, y1, x2, y2]
+                score = prediction[4]
+                category_id = int(prediction[5])
+                category_name = self.category_mapping[str(category_id)]
+
+                # fix negative box coords
+                bbox[0] = max(0, bbox[0])
+                bbox[1] = max(0, bbox[1])
+                bbox[2] = max(0, bbox[2])
+                bbox[3] = max(0, bbox[3])
+
+                # fix out of image box coords
+                if full_shape is not None:
+                    bbox[0] = min(full_shape[1], bbox[0])
+                    bbox[1] = min(full_shape[0], bbox[1])
+                    bbox[2] = min(full_shape[1], bbox[2])
+                    bbox[3] = min(full_shape[0], bbox[3])
+
+                # ignore invalid predictions
+                if not (bbox[0] < bbox[2]) or not (bbox[1] < bbox[3]):
+                    logger.warning(f"ignoring invalid prediction with bbox: {bbox}")
+                    continue
+
+                object_prediction = ObjectPrediction(
+                    bbox=bbox,
+                    category_id=category_id,
+                    score=score,
+                    bool_mask=None,
+                    category_name=category_name,
+                    shift_amount=shift_amount,
+                    full_shape=full_shape,
+                )
+                object_prediction_list.append(object_prediction)
+            object_prediction_list_per_image.append(object_prediction_list)
+
+        self._object_prediction_list_per_image = object_prediction_list_per_image
